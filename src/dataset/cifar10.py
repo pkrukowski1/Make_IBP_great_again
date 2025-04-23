Fix problem with incorrect computations

src/dataset/cifar10.py
@@ -1,9 +1,6 @@
 from torch.utils.data import Dataset
 from torchvision import datasets, transforms
 
-transform = transforms.Compose([
-    transforms.ToTensor(),
-])
 
 class CIFAR10(Dataset):
     """
@@ -14,7 +11,7 @@ class CIFAR10(Dataset):
     Attributes:
         data (torch.utils.data.Dataset): The CIFAR-10 dataset (either training or testing).
     Methods:
-        __init__(data_path: str = './data', train: bool = False):
+        __init__(data_path: str = './data', train: bool = False, flatten: bool = True):
             Initializes the CIFAR10 dataset wrapper by downloading and preparing
             either the training or testing dataset based on the `train` flag.
         __len__():
@@ -32,7 +29,11 @@ class CIFAR10(Dataset):
         sample = train_dataset[0]  # Get the first sample from the training dataset
     """
 
-    def __init__(self, data_path: str = './data', train: bool = False):
+    def __init__(self, data_path: str = './data', train: bool = False, flatten: bool = True):
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Lambda(lambda x: x.view(-1)) if flatten else lambda x: x
+        ])
         self.data = datasets.CIFAR10(root=data_path, train=train, download=True, transform=transform)
 
     def __len__(self):

src/dataset/mnist.py
@@ -1,26 +1,40 @@
 from torch.utils.data import Dataset
 from torchvision import datasets, transforms
 
-transform = transforms.Compose([
-    transforms.ToTensor(),
-])
 
 class MNIST(Dataset):
     """
-    A dataset class for handling MNIST data.
-
-    This class provides an interface to load and interact with the MNIST dataset. It supports both training and testing modes 
-    and allows easy access to individual samples.
-
-        data (Dataset): The MNIST dataset loaded from torchvision, either in training or testing mode.
-
+    MNIST Dataset Wrapper
+    This class provides a wrapper around the MNIST dataset, allowing for easy access
+    to either training or testing data. It supports indexing and provides the total length
+    of the dataset.
+    Attributes:
+        data (torch.utils.data.Dataset): The MNIST dataset (either training or testing).
+    Methods:
+        __init__(data_path: str = './data', train: bool = False, flatten: bool = True):
+            Initializes the MNIST dataset wrapper by downloading and preparing
+            either the training or testing dataset based on the `train` flag.
+        __len__():
             Returns the total number of samples in the dataset.
         __getitem__(idx: int):
-            Retrieves a sample (image and label) from the dataset based on the given index.
-
-        train (bool): A flag indicating whether to load the training dataset (True) or the testing dataset (False). Defaults to True.
+            Retrieves a single sample from the dataset based on the provided index.
+    Args:
+        data_path (str): The path where the MNIST dataset will be downloaded and stored.
+                         Defaults to './data'.
+        train (bool): A flag indicating whether to load the training dataset (True)
+                      or the testing dataset (False). Defaults to False.
+        flatten (bool): A flag indicating whether to flatten the images into 1D tensors.
+                        Defaults to True.
+    Usage:
+        train_dataset = MNIST(data_path='./data', train=True)
+        print(len(train_dataset))  # Total number of samples in the training dataset
+        sample = train_dataset[0]  # Get the first sample from the training dataset
     """
-    def __init__(self, data_path: str = './data', train: bool = False):
+    def __init__(self, data_path: str = './data', train: bool = False, flatten: bool = True):
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Lambda(lambda x: x.view(-1)) if flatten else lambda x: x
+        ])
         self.data = datasets.MNIST(root=data_path, train=train, download=True, transform=transform)
 
     def __len__(self):

src/dataset/svhn.py
@@ -1,9 +1,6 @@
 from torch.utils.data import Dataset
 from torchvision import datasets, transforms
 
-transform = transforms.Compose([
-    transforms.ToTensor(),
-])
 
 class SVHN(Dataset):
     """
@@ -11,7 +8,7 @@ class SVHN(Dataset):
     Attributes:
         data (torch.utils.data.Dataset): The dataset loaded from the SVHN dataset, either training or testing.
     Methods:
-        __init__(data_path: str = './data', train: bool = False):
+        __init__(data_path: str = './data', train: bool = False, flatten: bool = True):
             Initializes the SVHN dataset by downloading and preparing the specified split (train or test).
         __len__() -> int:
             Returns the total number of samples in the dataset.
@@ -22,7 +19,11 @@ class SVHN(Dataset):
         train (bool): Whether to load the training split. If False, loads the testing split. Defaults to False.
     """
 
-    def __init__(self, data_path: str= './data', train: bool = False):
+    def __init__(self, data_path: str= './data', train: bool = False, flatten: bool = True):
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Lambda(lambda x: x.view(-1)) if flatten else lambda x: x
+        ])
         _split = "train" if train else "test"
         self.data = datasets.SVHN(root=data_path, split=_split, download=True, transform=transform)
 

src/method/ibp.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn.functional as F
 import torch.nn as nn
 
-from typing import Tuple
+from typing import Tuple, List
 import logging
 
 from method.method_plugin_abc import MethodPluginABC
@@ -22,7 +22,7 @@ class IBP(MethodPluginABC):
         module (nn.Module): The neural network module through which intervals will be propagated.
         epsilon (torch.Tensor): The epsilon value used for interval propagation.
     Methods:
-        __init__() -> None:
+        __init__(epsilon: float) -> None:
             Initializes the IBP class with the given neural network module.
         forward(x: torch.Tensor, y: torch.Tensor) -> Interval:
             Performs interval bound propagation through the layers of the neural network.
@@ -35,37 +35,72 @@ class IBP(MethodPluginABC):
                 Interval: The propagated interval bounds.
     """
 
-    def __init__(self) -> None:
+    def __init__(self, epsilon: float) -> None:
         super().__init__()
 
-        log.info(f"IBP plugin initialized for epsilon")
+        self.epsilon = epsilon
+        self._layer_handlers = {}
+        self._register_handlers()
+
+        log.info(f"IBP plugin initialized for epsilon={self.epsilon}")
     
+    def _register_handlers(self):
+        """
+        Register handlers for different layer types.
+        """
+        self._layer_handlers: List[Tuple[type, callable]] = [
+            (nn.Linear, lambda layer, x, epsilon: IntervalLinear(
+                layer.in_features, layer.out_features
+            ).forward(x, epsilon, weight=layer.weight, bias=layer.bias)),
+
+            (nn.ReLU, lambda layer, x, epsilon: IntervalReLU().forward(x, epsilon)),
+
+            (nn.Conv2d, lambda layer, x, epsilon: IntervalConv2d(
+                in_channels=layer.in_channels,
+                out_channels=layer.out_channels,
+                kernel_size=layer.kernel_size,
+                stride=layer.stride,
+                padding=layer.padding,
+                dilation=layer.dilation,
+                groups=layer.groups,
+                padding_mode=layer.padding_mode
+            ).forward(x, epsilon, weight=layer.weight, bias=layer.bias)),
+
+            (nn.Flatten, lambda layer, x, epsilon: IntervalFlatten(
+                start_dim=layer.start_dim,
+                end_dim=layer.end_dim
+            ).forward(x, epsilon)),
+
+            ((nn.BatchNorm1d, nn.BatchNorm2d), lambda layer, x, epsilon: IntervalBatchNorm(
+                eps=layer.eps,
+                momentum=layer.momentum
+            ).forward(x, epsilon, layer.weight, layer.bias, layer.running_mean, layer.running_var, layer.training)),
+
+            (nn.MaxPool2d, lambda layer, x, epsilon: IntervalMaxPool2d(
+                kernel_size=layer.kernel_size,
+                stride=layer.stride,
+                padding=layer.padding,
+                dilation=layer.dilation
+            ).forward(x, epsilon)),
+
+            (nn.AvgPool2d, lambda layer, x, epsilon: IntervalAvgPool2d(
+                kernel_size=layer.kernel_size,
+                stride=layer.stride,
+                padding=layer.padding
+            ).forward(x, epsilon))
+        ]
+
     def forward(self, x: torch.Tensor, y: torch.Tensor) -> Interval:
         epsilon = self.epsilon * torch.ones_like(x)
-        layer_handlers = {
-            nn.Linear: lambda layer, x, epsilon: layer.forward(x, epsilon, layer.weight, layer.bias),
-            nn.ReLU: lambda layer, x, epsilon: layer.forward(x, epsilon),
-            nn.Conv2d: lambda layer, x, epsilon: layer.forward(x, epsilon, layer.weight, layer.bias),
-            nn.Flatten: lambda layer, x, epsilon: layer.forward(x, epsilon),
-            (nn.BatchNorm2d, nn.BatchNorm1d): lambda layer, x, epsilon: layer.forward(
-                x, epsilon, layer.weight, layer.bias,
-                running_mean=layer.running_mean,
-                running_var=layer.running_var,
-                training=self.module.training
-            ),
-            nn.MaxPool2d: lambda layer, x, epsilon: layer.forward(x, epsilon),
-            nn.AvgPool2d: lambda layer, x, epsilon: layer.forward(x, epsilon),
-        }
 
-        for layer in self.module.children():
-            for layer_type, handler in layer_handlers.items():
-                if isinstance(layer, layer_type):
-                    x, epsilon = handler(layer, x, epsilon)
-                    break
-            else:
-                log.warning(f"Layer {layer.__class__.__name__} not supported for IBP.")
-        return Interval(x-epsilon, x+epsilon)
-
+        with torch.no_grad():
+            for m in self.module.module.children():
+                for layer in m:
+                    for layer_type, handler in self._layer_handlers:
+                        if isinstance(layer, layer_type):
+                            x, epsilon = handler(layer, x, epsilon)
+                            break
+        return Interval(x - epsilon, x + epsilon)
 class IntervalLinear:
     """
     A class that performs interval-based linear transformations for interval arithmetic.
@@ -94,7 +129,7 @@ class IntervalLinear:
                     eps: torch.Tensor,
                     weight: torch.Tensor,
                     bias: torch.Tensor = None) -> Tuple[torch.Tensor, torch.Tensor]:
-        
+
         # Perform linear transformation
         new_mu = F.linear(
             input=mu,

src/network/mlp.py
@@ -77,8 +77,8 @@ class MLP(nn.Module):
         for i in range(last_layer_idx):
             modules.append(nn.Linear(layers[i], layers[i + 1]))
 
-            if i < last_layer_idx:
-                modules.append(activation_fnc())
+            if i < last_layer_idx-1:
+                modules.append(activation_fnc)
         return nn.Sequential(*modules)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -89,5 +89,5 @@ class MLP(nn.Module):
         Returns:
             torch.Tensor: The output tensor after passing through the model.
         """
-
+        
         return self.model(x)

src/network/utils.py
@@ -19,7 +19,8 @@ def load_model(model: nn.Module, weight_path: str, device: str = "cpu") -> None:
     """
     state_dict = torch.load(weight_path, map_location=device)
     state_dict = state_dict.get("state_dict", state_dict)
+
     if isinstance(state_dict, list):
         state_dict = state_dict[0]
-        state_dict = {f"linears.{int(key.split('.')[0])-1}.{key.split('.')[1]}": val for key, val in state_dict.items()}
-    model.load_state_dict(state_dict, strict=False)
+    state_dict = {f"{int(key.split('.')[0])-1}.{key.split('.')[1]}": val for key, val in state_dict.items()}
+    model.load_state_dict(state_dict, strict=True)