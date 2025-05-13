7eda07b5655973c286a3e2e0c4cdb1c71cf5baeb
Patryk Krukowski <patryk.krukowski@ideas-ncbr.pl>
2025-05-13 15:12:16 +0200

    Update __init__

--- src/network/__init__.py
+++ src/network/__init__.py
@@ -4,3 +4,4 @@ from .conv_small_mnist import ConvSmallMNIST
 from .conv_big_mnist import ConvBigMNIST
 from .conv_small_cifar import ConvSmallCIFAR
 from .conv_big_cifar import ConvBigCIFAR
+from .sdp_model_loader import SDPModelLoader
