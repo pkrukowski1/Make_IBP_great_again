f864458186caf773f85f118c94dc1f29b4ec3d41
Patryk Krukowski <patryk.krukowski@ideas-ncbr.pl>
2025-08-15 11:49:08 +0200

    Remove ResNet

--- src/network/__init__.py
+++ src/network/__init__.py
@@ -1,6 +1,5 @@
 # Pretrained
 from .mlp import MLP
-from .resnet5 import ResNet5
 from .conv_small_mnist import ConvSmallMNIST
 from .conv_big_mnist import ConvBigMNIST
 from .conv_small_cifar import ConvSmallCIFAR
