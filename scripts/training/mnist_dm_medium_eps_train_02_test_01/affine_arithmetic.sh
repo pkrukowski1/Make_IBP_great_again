8d6e7b30aa88d9134fa07ccd125dc86e691d74cc
Patryk Krukowski <patryk.krukowski@ideas-ncbr.pl>
2025-08-18 15:04:41 +0200

    Add scripts for MNIST dataset

---
+++ scripts/training/mnist_dm_medium_eps_train_02_test_01/affine_arithmetic.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=RAVEN_affine_arithmetic_training
+#SBATCH --qos=normal
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --partition=rtx4090
+
+source scripts/main.sh
+
+run_sweep_and_agent "scripts/training/mnist_dm_medium_eps_train_02_test_01/affine_arithmetic"
